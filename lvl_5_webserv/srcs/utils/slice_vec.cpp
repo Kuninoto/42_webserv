b3ed0adb496fc045693f0b54b443a9c343859510
ramos21rodrigo <ramos03rodrigo@gmail.com>
2023-04-10 21:42:23 +0100

    server_name , 404 page && non-blocking/keep-alive connection

---
+++ lvl_5_webserv/srcs/utils/slice_vec.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+/**
+ * @brief Creates a copy of vec in the range of [start_idx, end_idx]
+ *
+ * @param vec vector to slice
+ * @return slice of original vec
+ */
+template <typename T>
+std::vector<T> slice_vec(std::vector<T>& vec, unsigned int start_idx, unsigned int end_idx)
+{
+    typename std::vector<T>::iterator start_itr = vec.begin() + start_idx;
+    typename std::vector<T>::iterator end_itr = vec.begin() + end_idx + 1;
+
+    typename std::vector<T> result_vec(end_idx - start_idx + 1);
+    std::copy(start_itr, end_itr, result_vec.begin());
+
+    return result_vec;
+}
