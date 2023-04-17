6a91eab903e660d5a67f650631da288b1a2a53d8
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r13s13.42porto.com>
2023-04-17 17:58:16 +0100

    parsing keywords

--- lvl_5_webserv/includes/Server.hpp
+++ lvl_5_webserv/includes/Server.hpp
@@ -3,32 +3,35 @@
 
 # include <stdint.h>
 # include <string>
+# include <map>
 
 class Server {
     public:
-        Server(void) {};
+        Server(std::map<std::string, std::string>& parameters);
 		~Server(void) {};
 
         uint16_t getPort(void) const { return this->port; };
         const std::string& getHost(void) const { return this->host; };
+        const std::string& getIndex(void) const { return this->index; };
         const std::string& getServerName(void) const { return this->serverName; };
-        size_t getMaxBodySize(void) const { return this->maxBodySize; };
-
-		void setPort(uint16_t port) { this->port = port; };
-        void setHost(std::string& host) { this->host = host; };
-        void setServerName(std::string& serverName) { this->serverName = serverName; };
-        void setMaxBodySize(size_t maxBodySize) { this->maxBodySize = maxBodySize; };
+        const std::string& getRoot(void) const { return this->root; };
+        size_t getMaxBodySize(void) const { return this->clientMaxBodySize; };
 
     private:
         // 8080
         uint16_t port;
+        // localhost
+        std::string serverName;
         // 127.0.0.1
         std::string host;
-        // localhost
-        std::string	serverName;
         // 404 pages/error/404.html
-        std::string	errorPages;
-        size_t maxBodySize;
+        std::string errorPage;
+        std::string root;
+        std::string index;
+        size_t clientMaxBodySize;
+
+        std::map<std::string, std::string> parameters;
+
 };
 
 #endif // SERVER_HPP
